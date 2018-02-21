6a1b5909d66fe8336ac91dacd975ee0bdb74496a
tsasselli <tsasselli@gmail.com>
2018-02-21 11:17:59 -0800

    added random color generator, and functionality to loop through and update the ui

--- javascript/colorGame.js
+++ javascript/colorGame.js
@@ -1,11 +1,4 @@
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
@@ -21,10 +14,11 @@ rgbDisplay.textContent = pickedColor;
     squares[i].addEventListener("click", function() {
       // grab color of clicked squares
       var  clickedColor = this.style.backgroundColor;
-      changeColors(clickedColor);
+      console.log(clickedColor, pickedColor);
 
       if(clickedColor === pickedColor) {
         messageDisplay.textContent = "Correct!";
+        changeColors(clickedColor);
       } else {
       this.style.background = "#232323";
       messageDisplay.textContent = "Try Again";
@@ -34,11 +28,28 @@ rgbDisplay.textContent = pickedColor;
 
 function changeColors(color) {
   for(var i = 0; i < squares.length; i++) {
-    color[i].style.backgroundColor = color;
+    squares[i].style.background = color;
   }
 }
 
 function pickColor(){
  var random = Math.floor(Math.random() * colors.length);
  return colors[random];
+}
+
+function generateRandomColors(num){
+var arr = [];
+for(var i = 0; i <= num; i++){
+  //gets random colors and pushes it into array.
+  arr.push(randomColor());
+
+}
+return arr;
+}
+
+function randomColor(){
+  var red = Math.floor(Math.random() * 256);
+  var green = Math.floor(Math.random() * 256);
+  var blue = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${green}, ${blue})`
 }
